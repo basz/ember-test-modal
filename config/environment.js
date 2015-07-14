7e97df868435eb07e0b442d54ba0ab1aff5370d9
Bas Kamer <bas@bushbaby.nl>
2015-07-14 10:15:26 +0200

    enable pods

--- config/environment.js
+++ config/environment.js
@@ -3,6 +3,7 @@
 module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'ember-test-modal',
+    podModulePrefix: 'ember-test-modal/pods',
     environment: environment,
     baseURL: '/',
     locationType: 'auto',
